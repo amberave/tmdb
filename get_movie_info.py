4e40f5cdd343e95900755dc01256f155d9c640cb
Kate Wynter <kwynter013@gmail.com>
2025-12-07 23:47:13 +1100

    updating print statements

--- get_movie_info.py
+++ get_movie_info.py
@@ -30,33 +30,30 @@ if __name__ == "__main__":
     for movie_dict in movie_data:
         # get movie info
         # ENSURE BELOW IN SQUARE BRACKETS MATCH YOUR EXCEL
-        print(f"{movie_dict['Movie Title']} {movie_dict['Year']}")
+        print(f"Requesting info for: {movie_dict['Movie Title']} ({movie_dict['Year']})")
         movie_id = search_movie(movie_dict['Movie Title'], movie_dict['Year'])
         movie = tmdb.Movies(movie_id)
-        response = movie.info()
-        print(response)
-        input("Stop")
-        
-        movie_dict["Runtime (minutes)"] = movie.runtime
-        movie_dict["Budget"] = movie.budget
-        movie_dict["IMDB"] = movie.imdb_id
-        movie_dict["Country of Origin"] = movie.origin_country[0]
-
-        response = movie.releases()
-        print(response)
-        input("Stop")
-        for c in movie.countries:
-            if c['iso_3166_1'] == 'AU':
-                movie_dict["Classification"] = c['certification']
         
         response = movie.credits()
-        print(response)
-        input("Stop")
         directors = []  
         for credit in movie.crew:  
             if credit["job"] == "Director":  
                 directors.append(credit["name"])
         movie_dict["Director"] = ''.join(directors)
+        
+        # get basic movie info
+        response = movie.info()
+        movie_dict["Runtime (minutes)"] = movie.runtime
+        movie_dict["Budget"] = movie.budget
+        movie_dict["Box Office"] = movie.revenue
+        movie_dict["IMDB ID"] = movie.imdb_id
+        movie_dict["Country of Origin"] = movie.origin_country[0]
+
+        # get classification
+        response = movie.releases()
+        for c in movie.countries:
+            if c['iso_3166_1'] == 'AU':
+                movie_dict["Classification"] = c['certification']
     
     output_df = pd.DataFrame(movie_data)
     output_df.to_excel(f"{filename.replace('/', '/output-')}", index=False) 
