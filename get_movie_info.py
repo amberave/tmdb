d16e38548b4bbe914e41ef8e6bc021861c077da7
Kate Wynter <kwynter013@gmail.com>
2025-12-07 22:58:59 +1100

    initial commit

---
+++ get_movie_info.py
@@ -0,0 +1,55 @@
+import requests
+import tmdbsimple as tmdb
+import pandas as pd
+
+def search_movie(query):
+    search = tmdb.Search()
+    response = search.movie(query=query)
+    return search.results[0]["id"]
+
+def xl_to_dict(filename):
+    df = pd.read_excel(filename, header=0)
+    records_dict = df.to_dict('records')
+    return records_dict
+
+
+if __name__ == "__main__":
+    # set up API
+    tmdb.API_KEY = 'INSERT API KEY HERE'
+    tmdb.REQUESTS_TIMEOUT = 5  # seconds, for both connect and read
+    tmdb.REQUESTS_SESSION = requests.Session()
+    
+    # get excel data to read and use for search
+    filename = "demo_data.xlsx"
+    movie_data = xl_to_dict("demo_data.xlsx")
+    
+    # search and retrieve movies
+    for movie_dict in movie_data:
+        print(f"{movie_dict['Movie Title']} {movie_dict['Year']}")
+        movie_id = search_movie(f"{movie_dict['Movie Title']}")
+        #print(movie_id)
+        movie = tmdb.Movies(movie_id)
+        response = movie.info()
+        
+        movie_dict["Runtime (minutes)"] = movie.runtime
+        movie_dict["Budget"] = movie.budget
+        movie_dict["IMDB"] = movie.imdb_id
+        movie_dict["Country of Origin"] = movie.origin_country[0]
+
+        response = movie.releases()
+        for c in movie.countries:
+            if c['iso_3166_1'] == 'AU':
+                movie_dict["Classification"] = c['certification']
+        
+        response = movie.credits()
+        directors = []  
+        for credit in movie.crew:  
+            if credit["job"] == "Director":  
+                directors.append(credit["name"])
+        movie_dict["Director"] = ''.join(directors)
+        
+        
+         
+    
+    output_df = pd.DataFrame(movie_data)
+    output_df.to_excel(f"output-{filename}", index=False) 
