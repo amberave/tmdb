e0a9dbe59bed5bcb3c55eed200b934570ac98302
Kate Wynter <kwynter013@gmail.com>
2025-12-20 11:55:22 +1100

    changed letterboxd username

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from src.get_movie_info import get_movie_info
 
 if __name__ == "__main__":
     filename = "movies_database.xlsx"
-    letterboxd_username = "jigsaw4real"
+    letterboxd_username = "DWynter10"
     get_movie_info(filename, letterboxd_username)
